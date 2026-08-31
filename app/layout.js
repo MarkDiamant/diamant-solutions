e2bbb7322a1a062762814d338c978f096fc82842
MarkDiamant <markdiamant@gmail.com>
2026-08-31 22:06:06 +0100

    Use stable favicon asset

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import './globals.css';
 export const metadata = {
   title: 'Diamant Solutions | Websites & Business Solutions',
   description: 'Professional websites and bespoke business solutions built around your business.',
-  icons: { icon: '/DS Logo latest tagline.png', shortcut: '/DS Logo latest tagline.png', apple: '/DS Logo latest tagline.png' },
+  icons: { icon: '/icon.svg', shortcut: '/icon.svg', apple: '/icon.svg' },
 };
 
 export default function RootLayout({ children }) {
