0e6a7a0267ed37fd21d00673f62c779051ce9751
MarkDiamant <markdiamant@gmail.com>
2026-09-22 20:23:54 +0100

    Fix tenant settings API import path

--- app/api/business-software/tenants/[slug]/settings/route.js
+++ app/api/business-software/tenants/[slug]/settings/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { centralRest, tenantRecord } from "../../../../../lib/business-software/db";
+import { centralRest, tenantRecord } from "../../../../../../lib/business-software/db";
 
 export async function GET(_request,{params}){
   const slug=String((await params).slug||"").toLowerCase();
