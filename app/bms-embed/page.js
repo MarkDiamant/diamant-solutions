4ba68074a867aa11d62d6a9da23cd39136f8a1a5
MarkDiamant <markdiamant@gmail.com>
2026-09-23 00:21:17 +0100

    Switch BMS demo to exact isolated M&J UI mirror

--- app/bms-embed/page.js
+++ app/bms-embed/page.js
@@ -1,4 +1,2 @@
-import SoftwareDemo from "../software/SoftwareDemo";
-import "../software/software.css";
 export const metadata={title:"Business Software Demo",robots:{index:false,follow:false}};
-export default function BmsEmbed(){return <main className="softwarePage" style={{background:"#f3f7fa",padding:"4px"}}><SoftwareDemo/></main>}
+export default function BmsEmbed(){return <iframe src="https://mjmetal.diamantsolutions.co.uk/demo" title="Diamant Solutions Business Management Software demo" style={{display:"block",width:"100%",height:760,border:0,borderRadius:18,background:"#f5f5f2"}}/>;}
