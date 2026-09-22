6a5706c1290246eb8a9171edec1f86c5f5e01bc2
MarkDiamant <markdiamant@gmail.com>
2026-09-22 20:21:34 +0100

    Fix tenant integrations API import path

--- app/api/business-software/tenants/[slug]/integrations/route.js
+++ app/api/business-software/tenants/[slug]/integrations/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { centralRest, tenantRecord } from "../../../../../lib/business-software/db";
+import { centralRest, tenantRecord } from "../../../../../../lib/business-software/db";
 
 export async function GET(request,{params}){
   const slug=String((await params).slug||"").toLowerCase();
