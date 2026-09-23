a7d19e837fc3d8eb3b125561c85833e1a047b694
MarkDiamant <markdiamant@gmail.com>
2026-09-23 02:55:53 +0100

    Use DS-owned BMS runtime in embed

--- app/bms-embed/page.js
+++ app/bms-embed/page.js
@@ -1,2 +1,2 @@
 export const metadata={title:"Business Software Demo",robots:{index:false,follow:false}};
-export default function BmsEmbed(){return <iframe src="https://mjmetal.diamantsolutions.co.uk/demo" title="Diamant Solutions Business Management Software demo" style={{display:"block",width:"100%",height:760,border:0,borderRadius:18,background:"#f5f5f2"}}/>;}
+export default function BmsEmbed(){return <iframe src="/bms-runtime" title="Diamant Solutions Business Management Software demo" style={{display:"block",width:"100%",height:760,border:0,borderRadius:18,background:"#f5f5f2"}}/>;}
