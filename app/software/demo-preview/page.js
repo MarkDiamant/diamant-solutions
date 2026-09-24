3b50951c77394278815c152d2b870a601932fee7
MarkDiamant <markdiamant@gmail.com>
2026-09-24 20:54:07 +0100

    Add populated interactive demo preview route

---
+++ app/software/demo-preview/page.js
@@ -0,0 +1,3 @@
+import SoftwareDemo from '../SoftwareDemo';
+export const metadata={title:'Interactive BMS Demo',robots:{index:false,follow:false}};
+export default function DemoPreview(){return <main style={{margin:0,padding:0}}><SoftwareDemo/></main>}
